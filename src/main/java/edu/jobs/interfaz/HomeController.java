3246000ef1fedd2ab1873bad624f56e275461c8a
Joan Andrés <andres6936@live.com>
2020-06-19 14:11:19 -0500

    Support to HomeController

--- src/main/java/edu/jobs/interfaz/HomeController.java
+++ src/main/java/edu/jobs/interfaz/HomeController.java
@@ -1,14 +1,23 @@
 package edu.jobs.interfaz;
 
+import edu.jobs.mundo.Aspirante;
+import edu.jobs.mundo.Profession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/")
 public class HomeController
 {
-    @GetMapping("/")
-    public String home()
+    @GetMapping
+    public String showHomepage(Model model)
     {
+        model.addAttribute("applicant", new Aspirante(
+                "Joan", Profession.SOFTWARE_ENGINEER, 4, 22,
+                "319 656 9458", "data/andres.jpg"));
+
         return "home";
     }
 }
