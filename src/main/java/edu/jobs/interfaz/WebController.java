b217fca26ed95858211aaa29ee158016a06a5a3e
Joan Andrés <andres6936@live.com>
2020-06-20 13:10:30 -0500

    Added news aspirantes

--- src/main/java/edu/jobs/interfaz/WebController.java
+++ src/main/java/edu/jobs/interfaz/WebController.java
@@ -20,6 +20,30 @@ public class WebController
                 10, 36, "2145500",
                 "static/img/Bill-Clinton.jpg"));
 
+        repository.save(new Aspirante("Carlos Méndez", Profession.SOFTWARE_ENGINEER,
+                5, 31, "2669966",
+                "static/img/Christopher-Nolan.jpg"));
+
+        repository.save(new Aspirante("Andrés Pérez", Profession.ADMINISTRATOR,
+                1, 25, "4296831",
+                "static/img/Gordon-Ramsay.jpg"));
+
+        repository.save(new Aspirante("Marcela Otero", Profession.ADMINISTRATOR,
+                3, 29, "2174157",
+                "static/img/Jennifer-Lawrence.jpg"));
+
+        repository.save(new Aspirante("Jorge Campos", Profession.SOFTWARE_ENGINEER,
+                8, 40, "2774589",
+                "static/img/Kevin-Spacey.jpg"));
+
+        repository.save(new Aspirante("Ana Rojas", Profession.ECONOMIST,
+                1, 22, "3526478",
+                "static/img/Kate-Mara.jpg"));
+
+        repository.save(new Aspirante("Juan Rodríguez", Profession.INDUSTRIAL_ENGINEER,
+                6, 32, "4158877",
+                "static/img/Liam-Neeson.jpg"));
+
         return "successful";
     }
 }
