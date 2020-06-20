edbec437ce9ad0177a4f9b64732a4b323636cb97
Joan Andrés <andres6936@live.com>
2020-06-20 18:25:41 -0500

    Added block each for show the option based in the values of enum

--- src/main/java/edu/jobs/interfaz/AddController.java
+++ src/main/java/edu/jobs/interfaz/AddController.java
@@ -1,6 +1,8 @@
 package edu.jobs.interfaz;
 
+import edu.jobs.mundo.Profession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,8 +11,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class AddController
 {
     @GetMapping
-    public String showAddPage()
+    public String showAddPage(Model model)
     {
+        model.addAttribute("professions", Profession.values());
+
         return "add";
     }
 }
