07fda36b6f078c8c0d3fe26f13903086e8b7cca9
Joan Andrés <andres6936@live.com>
2020-06-20 17:21:39 -0500

    Added new controller for add new applicants

---
+++ src/main/java/edu/jobs/interfaz/AddController.java
@@ -0,0 +1,16 @@
+package edu.jobs.interfaz;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/add")
+public class AddController
+{
+    @GetMapping
+    public String showAddPage()
+    {
+        return "add";
+    }
+}
